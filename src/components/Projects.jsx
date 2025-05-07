bd81c8857449503b0da0c397be0f27517c2cb118
Mika Vohl <mikavohl@gmail.com>
2025-05-06 22:55:45 -0400

    Made projects clickable

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,24 +7,26 @@ export default function Projects() {
 
       <div className="grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         {projects.map((p) => (
-          <div
-            key={p.id}
-            className="rounded-lg border border-gray-200 bg-white p-6 shadow-sm"
-          >
-            <h3 className="text-2xl font-semibold">
-              {p.title}
-            </h3>
-            <h4 className="mb-2 text-lg font-medium text-gray-500">
-              {p.stack}
-            </h4>
-            <div className="h-48 w-full overflow-hidden rounded-md">
-              <img
-                src={p.img}
-                alt={p.title}
-                className="h-full w-full object-contain"
-              />
+          <a href={p.href} target="_blank">
+            <div
+              key={p.id}
+              className="rounded-lg border border-gray-200 bg-white p-6 shadow-sm"
+            >
+              <h3 className="text-2xl font-semibold">
+                {p.title}
+              </h3>
+              <h4 className="mb-2 text-lg font-medium text-gray-500">
+                {p.stack}
+              </h4>
+              <div className="h-48 w-full overflow-hidden rounded-md">
+                <img
+                  src={p.img}
+                  alt={p.title}
+                  className="h-full w-full object-contain"
+                />
+              </div>
             </div>
-          </div>
+          </a>
         ))}
       </div>
     </section>
